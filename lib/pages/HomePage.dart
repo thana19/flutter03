a578d7df5e6a1095d7d38b4cc8868317eef46aed
thana19 <thana.j@gmail.com>
2021-06-21 23:07:42 +0700

    create menu

--- lib/pages/HomePage.dart
+++ lib/pages/HomePage.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter03/widgets/menu.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
@@ -11,6 +12,7 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+        drawer: Menu(),
         appBar: AppBar(
           title: Text('HomePage'),
         )
