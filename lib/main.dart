33f382ecd9c2ef64dc1151364bf9f32ecde526aa
thana19 <thana.j@gmail.com>
2021-06-21 17:27:47 +0700

    create homepage.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter03/pages/HomePage.dart';
 import 'package:flutter03/pages/MyHomePage.dart';
 
 void main() {
@@ -16,7 +17,11 @@ class MyApp extends StatelessWidget {
 
         primarySwatch: Colors.blue,
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      // home: MyHomePage(title: 'Flutter Demo Home Page'),
+      initialRoute: '/',
+      routes: {
+        '/' : (context) => HomePage()
+      },
     );
   }
 }
